869555837ee61871ba2619cbf288bb6f20126921
Grigory Pervakov <pervakov.grigory@gmail.com>
2019-03-30 21:23:37 +0300

    Base StartMenu code.

--- src/game/scenes/menu/StartMenu.cpp
+++ src/game/scenes/menu/StartMenu.cpp
@@ -1,11 +1,30 @@
 #include "StartMenu.h"
-#include <GL/glew.h>
+
+#include <game/GlobalContext.h>
+#include <utils/resourseManager.h>
+
+StartMenu::StartMenu() : camera(GlobalContext::SCREEN_WIDTH, GlobalContext::SCREEN_HEIGHT) {
+  init();
+}
 
 void StartMenu::init() {
+  proj_location = glGetUniformLocation(ResourseManager::simpleShader->Program, "u_ProjTrans");
+  model_location = glGetUniformLocation(ResourseManager::simpleShader->Program, "u_ModelTrans");
 }
-void StartMenu::enter() {}
-void StartMenu::update() {}
+
+void StartMenu::enter() {
+
+}
+
+void StartMenu::update() {
+  camera.update();
+}
+
 void StartMenu::draw(float delta) {
+  ResourseManager::simpleShader->Use();
+  glUniformMatrix4fv(proj_location, 1, GL_FALSE, glm::value_ptr(camera.proj));
+  batch.draw(*ResourseManager::menuTexture.get(), model_location,
+    glm::vec2{ 0, 0 }, GlobalContext::SCREEN_WIDTH, GlobalContext::SCREEN_HEIGHT);
 }
 
 std::unique_ptr<Scene> createStartMenu() {
--- src/utils/resourseManager.cpp
+++ src/utils/resourseManager.cpp
@@ -5,24 +5,13 @@ std::unique_ptr<fjfj::Mesh> ResourseManager::squareMesh;
 
 std::unique_ptr <fjfj::Shader> ResourseManager::simpleShader;
 
-std::unique_ptr <fjfj::Texture> ResourseManager::wallTexture;
-std::unique_ptr <fjfj::Texture> ResourseManager::floorTexture;
+std::unique_ptr <fjfj::Texture> ResourseManager::menuTexture;
+std::unique_ptr <fjfj::Texture> ResourseManager::startButtonTexture;
 
 void ResourseManager::initResourses() {
-  GLfloat vert[] = {
-    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-    0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-    0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
-
-    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-    -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-    0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
-  };
-
-  squareMesh = std::make_unique<fjfj::Mesh>(fjfj::Mesh(vert, 6));
-
   simpleShader = std::make_unique<fjfj::Shader>("assets/shader/sample.vert", "assets/shader/sample.frag");
 
-  wallTexture = std::make_unique<fjfj::Texture>("assets/texture/wall.png");
-  floorTexture = std::make_unique<fjfj::Texture>("assets/texture/floor.png");
+
+  menuTexture = std::make_unique<fjfj::Texture>("assets/texture/menuScene.png");
+  startButtonTexture = std::make_unique<fjfj::Texture>("assets/texture/floor.png");
 }
